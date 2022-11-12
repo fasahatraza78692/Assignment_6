02e86fab88cfa429877215666c750e68e89ac120
Mohd Fasahat Raza <115611199+fasahatraza78692@users.noreply.github.com>
2022-11-12 20:37:21 +0530

    Update Question_8.c

--- Assignment_6/Question_8.c
+++ Assignment_6/Question_8.c
@@ -4,10 +4,10 @@ int main()
 	int n,i;
 	printf("Enter a number\n");
 	scanf("%d",&n);
-	for(i=2;i<n/2;i++)
+	for(i=2;i<=n/2;i++)
 	  	if(n%i==0) 
 			break;
-	  if(i==n/2)
+	  if(i==n/2+1)
 	     printf("%d is a prime number",n);
 	  else
 	     printf("%d is not a prime number",n);
